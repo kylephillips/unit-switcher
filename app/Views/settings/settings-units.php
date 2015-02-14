a61b64add498b3e73f34e1028bf5d89bde3a2efa
Kyle Phillips <kylephillipsdesign@gmail.com>
2015-02-14 16:39:19 -0500

    Styling for unit settings.

--- app/Views/settings/settings-units.php
+++ app/Views/settings/settings-units.php
@@ -1,16 +1,48 @@
 <?php settings_fields( 'unit-switcher-units' ); ?>
 <tr valign="top">
-	<td colspan="2">
+	<td colspan="2" style="padding:0;">
 		<div class="unitswitcher-units">
-			<ul>
+			<ul class="unitswitcher-units-header">
+				<li><?php _e('Primary Unit', 'unitswitcher'); ?> (<span>X</span>)</li>
+				<li><?php _e('Alternate Units', 'unitswitcher'); ?></li>
+			</ul>
+
+			<ul class="unitswitcher-unit-list">
 				<li>
-					<div class="name">
-						<input type="text" name="unitswitcher_units[miles][default]" value="miles">
-					</div> 
-					<div class="multiplier">
-						<input type="text" name="unitswitcher_units[miles][formula]" value="*">
+					<table class="unitswitcher-unit-table">
+						<tr>
+							<td>
+								<div class="unitswitcher-defaults">
+									<input type="text" placeholder="<?php _e('Name', 'unitswitcher'); ?>">
+									<input type="text" placeholder="<?php _e('Singular Name', 'unitswitcher'); ?>">
+								</div>
+							</td>
+							<td>
+								<ul class="unitswitcher-alternates">
+									<li>
+										<div class="formula">
+											<input type="text" class="unitswitcher-formula" placeholder="<?php _e('Formula', 'unitswitcher'); ?>">
+											<span class="equals">=</span>
+										</div>
+										<div class="alternates">
+											<input type="text" placeholder="<?php _e('Name', 'unitswitcher'); ?>">
+											<input type="text" placeholder="<?php _e('Singular Name', 'unitswitcher'); ?>">
+										</div>
+										<div class="unitswitcher-add-remove-unit unitswitcher-btn-group">
+											<a href="#" class="unitswitcher-remove-alternate">-</a>
+											<a href="#" class="unitswitcher-add-alternate">+</a>
+										</div>
+									</li>
+								</ul>
+							</td>
+						</tr>
+					</table>
+					<div class="unitswitcher-add-remove-unit unitswitcher-btn-group">
+						<a href="#" class="unitswitcher-remove-unit">-</a>
+						<a href="#" class="unitswitcher-add-unit">+</a>
 					</div>
 				</li>
+
 			</ul>
 		</div>
 	</td>
